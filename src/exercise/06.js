3e2358adc79aa16e2930ce369c13717751351fcd
mcallec1 <marc.callec@gmail.com>
2022-05-21 16:24:37 +0930

    Exercise 06 - Extra 4

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -13,6 +13,38 @@ import {
   PokemonDataView,
 } from '../pokemon'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {hasError: false, message: null}
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return {hasError: true, message: error.message}
+  }
+
+  // componentDidCatch(error, errorInfo) {
+  //   // You can also log the error to an error reporting service
+  //   logErrorToMyService(error, errorInfo)
+  // }
+
+  render() {
+    if (this.state.hasError) {
+      console.log('this.state', this.state)
+      // You can render any custom fallback UI
+      return (
+        <div role="alert">
+          There was an error:{' '}
+          <pre style={{whiteSpace: 'normal'}}>{this.state.message}</pre>
+        </div>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
 function PokemonInfo({pokemonName}) {
   const [state, setState] = useState({
     pokemon: null,
@@ -25,24 +57,19 @@ function PokemonInfo({pokemonName}) {
 
     setState({status: 'pending'})
     fetchPokemon(pokemonName)
-      .then(pokemonData => {
-        setState({status: 'resolved', pokemon: pokemonData})
+      .then(pokemon => {
+        setState({status: 'resolved', pokemon})
       })
       .catch(error => {
-        setState({status: 'rejected', error: error})
+        setState({status: 'rejected', error})
       })
   }, [pokemonName])
 
   if (state.status === 'idle') return 'Submit a pokemon'
   if (state.status === 'pending')
     return <PokemonInfoFallback name={pokemonName} />
-  if (state.status === 'rejected')
-    return (
-      <div role="alert">
-        There was an error:{' '}
-        <pre style={{whiteSpace: 'normal'}}>{state.error.message}</pre>
-      </div>
-    )
+  if (state.status === 'rejected') throw new Error(state.error.message)
+
   if (state.status === 'resolved') <PokemonDataView pokemon={state.pokemon} />
 }
 function App() {
@@ -57,7 +84,9 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary>
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   )
